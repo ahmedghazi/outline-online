fd03963618cac42483f74183e1b9df0c1edc6f2f
ahmedghazi <atmet.ghazi@gmail.com>
2024-07-26 10:28:10 +0200

    let s gooo

--- web/app/components/modules/ImageUI.tsx
+++ web/app/components/modules/ImageUI.tsx
@@ -17,7 +17,7 @@ const ModuleImageUI = ({ input }: Props): JSX.Element => {
     <div
       className={clsx(
         "module module--image",
-        input.size && `md:col-span-${input.size}`
+        input.size && `md:col-span-${input.size === 1 ? 4 : input.size}`
       )}>
       {input.image && <Figure asset={input.image?.image?.asset} width={1000} />}
       <figcaption className='py-2'>{input.image?.caption}</figcaption>
