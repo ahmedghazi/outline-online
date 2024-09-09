5d2358a41c0ef944d8ad8b75b8ea70e1f8a83ba1
ahmedghazi <atmet.ghazi@gmail.com>
2024-09-09 11:40:37 +0200

    let s gooo

--- web/app/components/Trinket.jsx
+++ web/app/components/Trinket.jsx
@@ -1,13 +1,14 @@
 "use client";
 import React, { useRef, useState } from "react";
 import { useLoader, useFrame, useThree } from "@react-three/fiber";
-import { useGLTF } from "@react-three/drei";
+import { useGLTF, Float, DragControls } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 // import { TransformControls } from "@react-three/drei";
 import { proxy, useSnapshot } from "valtio";
 import { useRouter } from "next/navigation";
 import { _linkResolver, _randomNum } from "../utils/utils";
 import { useDrag } from "react-use-gesture";
+// import { DragControls } from "three/examples/jsm/controls/DragControls";
 
 const state = proxy({ current: null, mode: 0 });
 
@@ -24,25 +25,27 @@ const Trinket = (props) => {
   //   props.position[1],
   //   0,
   // ]);
-  const [position, setPosition] = useState([0, 0, 0]);
+  // const [position, setPosition] = useState([0, 0, 0]);
+  const [position, setPosition] = useState(props.position);
   const { size, viewport } = useThree();
   // console.log(size, viewport);
   const aspect = size.width / viewport.width;
   // Subscribe this component to the render-loop, rotate the mesh every frame
 
-  useFrame(() => {
-    groupRef.current.rotation.z += 0.001;
-    groupRef.current.rotation.x += 0.001;
-  });
+  // useFrame(() => {
+  //   groupRef.current.rotation.x += 0.0005;
+  //   groupRef.current.rotation.y += 0.001;
+  // });
 
   // Return view, these are regular three.js elements expressed in JSX
   // console.log(props);
   const bind = useDrag(
     ({ offset: [x, y] }) => {
-      // console.log(position);
+      console.log(x, position, aspect);
       // console.log(aspect);
       const [, , z] = position;
-      setPosition([x / aspect, -y / aspect, z]);
+      // setPosition([x / aspect, -y / aspect, z]);
+      // setPosition([x / 20, -y / 20, z]);
     },
     { pointerEvents: true }
   );
@@ -56,16 +59,21 @@ const Trinket = (props) => {
   };
   return (
     <>
-      <group
-        ref={groupRef}
-        position={position}
-        {...bind()}
-        onClick={_onClick}
-        // onPointerMove={(e) => setPosition(e.point.x, e.point.y, 0)}
-        onPointerOver={(e) => console.log("hover", e.x)}
-        onPointerOut={(e) => console.log("unhover")}>
-        <primitive object={scene} scale={1} name={name} />
-      </group>
+      <Float speed={1.4} rotationIntensity={1.5} floatIntensity={2.3}>
+        <DragControls>
+          <group
+            ref={groupRef}
+            position={position}
+            // {...bind()}
+            onClick={_onClick}
+            // onPointerMove={(e) => setPosition(e.point.x, e.point.y, 0)}
+            onPointerOver={(e) => console.log("hover", e.x)}
+            onPointerOut={(e) => console.log("unhover")}>
+            <boxGeometry args={[1, 1, 1]} />
+            <primitive object={scene} scale={1} name={name} />
+          </group>
+        </DragControls>
+      </Float>
     </>
   );
 };
--- web/app/components/Trinkets.jsx
+++ web/app/components/Trinkets.jsx
@@ -3,7 +3,8 @@ import React, { Suspense, useRef, useState } from "react";
 // import { Obj3d } from "../types/schema";
 
 import { Canvas, useThree } from "@react-three/fiber";
-import { OrbitControls, Stage } from "@react-three/drei";
+import { Float, OrbitControls, Stage } from "@react-three/drei";
+import { PerspectiveCamera } from "three";
 import Trinket from "./Trinket";
 // import { proxy, useSnapshot } from "valtio";
 // const state = proxy({ current: null, mode: 0 });
@@ -30,18 +31,36 @@ import Trinket from "./Trinket";
 
 const Trinkets = (props) => {
   const ref = useRef();
-  const space = 5;
+  const space = 1;
+  //fov
+  //aspect-ratio: 16/9 => 1.77
+
+  function CameraHelper() {
+    const camera = new PerspectiveCamera(60, 1, 1, 3);
+    return <cameraHelper args={[camera]} />;
+  }
   return (
     <section className='tinkets'>
       <Canvas
         shadows
-        dpr={[1, 2]}
+        // dpr={[1, 2]}
         // camera={{ fov: 50 }}
-        camera={{ fov: 35, zoom: 1, near: 1, far: 1000 }}
+        camera={{ fov: 60, zoom: 1, near: 1, far: 1000 }}
+        // orthographic
+        // camera={{
+        //   position: [0, 0, 20],
+        //   fov: 50,
+
+        // }}
+        // orthographic
+
         style={{
           width: "calc(var(--vw) * 100)",
           height: "calc(var(--vh) * 100)",
         }}>
+        <ambientLight />
+        <pointLight position={[5, 5, 5]} intensity={3} />
+        <pointLight position={[-3, -3, 2]} />
         <Suspense fallback={null}>
           <Stage
             controls={ref}
@@ -51,15 +70,16 @@ const Trinkets = (props) => {
             environment='city'>
             {props.input.map((item, i) => (
               <Trinket
+                key={i}
                 input={item}
                 name={i + 10}
-                key={i}
                 position={[Math.random() * space, Math.random() * space, 0]}
-                scale={1}
+                // scale={1}
               />
             ))}
           </Stage>
         </Suspense>
+        <CameraHelper />
         {/* <Controls /> */}
         {/* <OrbitControls ref={ref} makeDefault={false} /> */}
       </Canvas>
