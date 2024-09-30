af7ea6c0c059a23115e9f66e55503b5f667ea53a
ahmedghazi <atmet.ghazi@gmail.com>
2024-09-30 13:33:55 +0200

    let s gooo

--- web/app/utils/env.ts
+++ web/app/utils/env.ts
@@ -1,5 +1,5 @@
 /**
- * .
+ *
  * As this file is reused in several other files, try to keep it lean and small.
  * Importing other npm packages here could lead to needlessly increasing the client bundle size, or end up in a server-only function that don't need it.
  */
