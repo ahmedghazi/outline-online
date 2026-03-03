924f8490d59f6fad2b9c1757539afda5b5637b5a
ahmedghazi <atmet.ghazi@gmail.com>
2026-03-03 20:16:08 +0100

    error msg

--- web/app/components/shop/PostCheckout.tsx
+++ web/app/components/shop/PostCheckout.tsx
@@ -52,7 +52,7 @@ const CheckoutError = () => {
       <div className='header mb-1e !p-0'>
         <h1>Error :(</h1>
       </div>
-      <p>please contact us to investigate the error.</p>
+      <p>Please contact us to investigate the error.</p>
       {/* <p>display some error message here</p> */}
       <a href='mailto:hello@outlineonline.com'>Contact us</a>
     </div>
