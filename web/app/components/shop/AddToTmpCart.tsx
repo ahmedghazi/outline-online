588ce8f761cc68661c9aecf7db437856dda8cdaf
ahmedghazi <atmet.ghazi@gmail.com>
2026-03-05 18:50:50 +0100

    diff prod with diff license

--- web/app/components/shop/AddToTmpCart.tsx
+++ web/app/components/shop/AddToTmpCart.tsx
@@ -135,7 +135,7 @@ const AddToTmpCart = ({
       if (exist.length === 0)
         setTmpProducts({ type: "ADD", payload: _productData });
       else {
-        setTmpProducts({ type: "REPLACE", payload: _productData });
+        // setTmpProducts({ type: "REPLACE", payload: _productData });
       }
     } else {
       const isInTmpProducts = tmpProducts.some(
